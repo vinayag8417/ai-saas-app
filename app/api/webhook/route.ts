a1d1eea8307c2bf5c1d7818a56bd36f236c36e24
Vinay Agarwal <vinayagarwal8417@gmail.com>
2025-10-08 01:17:52 +0530

    Update route.ts

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-	apiVersion: "2024-09-30.acacia",
+	apiVersion: "2025-02-24.acacia",
 });
 
 export async function POST(req: NextRequest) {
