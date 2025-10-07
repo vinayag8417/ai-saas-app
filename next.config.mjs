ec4b002a539b7b89359d725f549d102d5e557038
Vinay <vinayagarwal8417@gmail.com>
2025-10-08 01:38:00 +0530

    Update next.config.mjs to ensure path resolution

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				hostname: "cdn-icons-png.flaticon.com",
-			},
-		],
-	},
+    images: {
+        remotePatterns: [
+            {
+                hostname: "cdn-icons-png.flaticon.com",
+            },
+        ],
+    },
+    webpack: (config, { isServer }) => {
+        // Ensure @ alias is resolved
+        config.resolve.alias = {
+            ...config.resolve.alias,
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
